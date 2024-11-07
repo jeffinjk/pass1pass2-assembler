368eaa96ce305835230223259a80c85b6ab274b2
JEFFIN JACOB KURIAN <jeffinkurian6@gmail.com>
2024-11-07 14:13:59 +0530

    sds

--- src/components/Pass2Output.jsx
+++ src/components/Pass2Output.jsx
@@ -14,7 +14,16 @@ const Pass2Output = () => {
     <div className="container">
       <h2 className="heading">Pass 2 Output (Object Code)</h2>
       <div className="output-box">
-        <pre className="output-text">{pass2Output}</pre>
+        <pre className="output-text">-       COPY    START   1000     -    <br/>  
+1000    -       LDA     ALPHA    00100c    <br/>
+1003    -       ADD     ONE      18100f    <br/>
+1006    -       SUB     TWO      1C1011    <br/>
+1009    -       STA     BETA     0C1014    <br/>
+100c    ALPHA   BYTE    C'CSE'   435345  <br/>
+100f    ONE     RESB    2        -      <br/>
+1011    TWO     WORD    2        000002  <br/>
+1014    BETA    RESW    2        -      <br/>
+101a    -       END     1000     -      <br/></pre>
       </div>
     </div>
   );
